7c1beaf7a1551674154fa7f99a58696a6ce0669e
Arun MR <arunmr@beaverlodge.in>
2022-02-07 17:39:34 +0530

    Update lambda_build.sh

--- lambda_build.sh
+++ lambda_build.sh
@@ -1,7 +1,3 @@
 rm api-key-gen.zip
 zip -vrj api-key-gen src/* -i '*.py'
-#zip -u api-key-gen.zip lambda_function.py
-#cd packages
-#zip -r9 ../fee.zip .
-#cd ..
 aws lambda update-function-code --function-name api-token-generator --zip-file fileb://api-key-gen.zip
